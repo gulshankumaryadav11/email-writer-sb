3e80dce99981ccd3bb012f6670b8020dbe0edb44
gulshankumaryadav11 <yadavgulshankumar507@gmail.com>
2026-05-13 00:16:15 +0530

    Update and rename ResendEmailService.java to EmailService.java

--- src/main/java/com/email/email_writer_sb/service/EmailService.java
+++ src/main/java/com/email/email_writer_sb/service/EmailService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ResendEmailService {
+public class EmailService {
 
     @Value("${resend.api.key}")
     private String resendApiKey;
