b3644eee587c304a4f026a625cc2cd6ac2f45100
gulshankumaryadav11 <yadavgulshankumar507@gmail.com>
2026-05-13 00:48:34 +0530

    Enhance OTP email sending with error handling
    Refactor sendOtpEmail method to include error handling and update email content.

--- src/main/java/com/email/email_writer_sb/service/ResendEmailService.java
+++ src/main/java/com/email/email_writer_sb/service/ResendEmailService.java
@@ -1,6 +1,7 @@
 package com.email.email_writer_sb.service;
 
 import com.resend.Resend;
+import com.resend.core.exception.ResendException;
 import com.resend.services.emails.model.CreateEmailOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,15 +14,26 @@ public class ResendEmailService {
 
     public void sendOtpEmail(String to, String otp) {
 
-        Resend resend = new Resend(resendApiKey);
+        try {
 
-        CreateEmailOptions params = CreateEmailOptions.builder()
-                .from("onboarding@resend.dev")
-                .to(to)
-                .subject("Your OTP Code")
-                .html("<h2>Your OTP is: " + otp + "</h2>")
-                .build();
+            Resend resend = new Resend(resendApiKey);
 
-        resend.emails().send(params);
+            CreateEmailOptions params = CreateEmailOptions.builder()
+                    .from("onboarding@resend.dev")
+                    .to(to)
+                    .subject("Your OTP Code")
+                    .html(
+                            "<h2>Email Verification</h2>" +
+                            "<p>Your OTP is:</p>" +
+                            "<h1>" + otp + "</h1>" +
+                            "<p>This OTP expires in 5 minutes.</p>"
+                    )
+                    .build();
+
+            resend.emails().send(params);
+
+        } catch (ResendException e) {
+            throw new RuntimeException("Failed to send OTP email");
+        }
     }
 }
